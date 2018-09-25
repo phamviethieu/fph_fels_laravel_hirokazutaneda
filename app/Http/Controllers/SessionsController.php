a62a1e4b94620b08220ed894beb685f054369971
Hirokazu Taneda <hirokazu.taneda.framgia@gmail.com>
2018-09-25 10:07:10 +0800

    Fix File Name

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -14,7 +14,7 @@ class SessionsController extends Controller
 
     public function create()
     {
-        return view('sessions.create');
+        return view('sessions.login');
     }
 
     public function store()
